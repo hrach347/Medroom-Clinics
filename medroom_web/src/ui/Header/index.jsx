aa1dcc97a7f1ee1fdb1de2c24aefa75bd3e94e8e
hrach347 <hrachpapyan347@gmail.com>
2025-03-04 23:28:55 +0400

    header without logo

--- medroom_web/src/ui/Header/index.jsx
+++ medroom_web/src/ui/Header/index.jsx
@@ -1,13 +1,20 @@
-import {Link} from "react-router"
+import { Link } from "react-router";
 import styles from "./style.module.css";
 
 function Header() {
-  return (<div className={styles.container}>
+  return (
+    <div className={styles.container}>
+      <div className={styles.logoContainer}>
+        <img src="" alt=""/>
+      </div>
+      <div className={styles.menu}>
         <Link to="/">Home</Link>
         <Link to="/Contact">Contact</Link>
         <Link to="/About">About</Link>
         <Link to="/Services">Services</Link>
-  </div>);
+      </div>
+    </div>
+  );
 }
 
 export default Header;
