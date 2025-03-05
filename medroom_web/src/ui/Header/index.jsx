9e270f209fa10ce52fe928177370fefb393e3cd2
hrach347 <hrachpapyan347@gmail.com>
2025-03-05 09:54:31 +0400

    header media

--- medroom_web/src/ui/Header/index.jsx
+++ medroom_web/src/ui/Header/index.jsx
@@ -1,13 +1,41 @@
 import { Link } from "react-router";
+
+import logo from "../../Assets/Images/logo.svg";
 import styles from "./style.module.css";
+import { useState } from "react";
 
 function Header() {
+  const [isOpened, setIsOpened] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpened(!isOpened);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.logoContainer}>
-        <img src="" alt="" />
+        <img src={logo} alt="" />
       </div>
       <div className={styles.menu}>
+        <Link to="/">Home</Link>
+        <Link to="/About">About</Link>
+        <Link to="/Services">Services</Link>
+        <Link to="/Staff">Staff</Link>
+        <Link to="/Contact">Contact</Link>
+      </div>
+      <div
+        className={`${styles.menuIcon} ${isOpened ? styles.openedIcon : ""}`}
+        onClick={toggleMenu}
+      >
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div
+        className={`${styles.mobileMenu} ${
+          isOpened ? styles.openedMobileMenu : ""
+        }`}
+      >
         <Link to="/">Home</Link>
         <Link to="/About">About</Link>
         <Link to="/Services">Services</Link>
