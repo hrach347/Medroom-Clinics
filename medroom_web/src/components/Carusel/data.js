d540db3a31397b21f46b10c43336d6f17e15d17e
SaqoAxayan <saqoaxayan31@gmail.com>
2025-03-12 03:57:42 +0400

    finish carusel

--- medroom_web/src/components/Carusel/data.js
+++ medroom_web/src/components/Carusel/data.js
@@ -1,9 +1,32 @@
+import React from "react";
+import styles from "./style.module.css";
+import leftArrow from "../../Assets/Images/arrow1.png";
+import rightArrow from "../../Assets/Images/arrow2.png";
+
+const PrevArrow = ({ onClick }) => {
+  return (
+    <button alt="Previous" onClick={onClick}>
+      <img src={rightArrow} alt="" />
+    </button>
+  );
+};
+
+const NextArrow = ({ onClick }) => {
+  return (
+    <button alt="Next" onClick={onClick}>
+      <img src={leftArrow} alt="" />
+    </button>
+  );
+};
+
 export const settings = {
   dots: true,
   infinite: true,
   speed: 500,
   slidesToShow: 3,
   slidesToScroll: 1,
+  prevArrow: <PrevArrow />,
+  nextArrow: <NextArrow />,
 
   responsive: [
     {
--- medroom_web/src/ui/Blogs/data.js
+++ medroom_web/src/ui/Blogs/data.js
@@ -4,29 +4,29 @@ export const data = [
   {
     url: clinics,
     title:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    description: "What is Lorem Ipsum?",
+      "Մենք առաջարկում ենք մազերի փոխպատվաստման որակյալ ծառայություն, որը կօգնի ձեզ վերագտնել ձեր ինքնավստահությունը",
+    description: "Մազերի փոխպատվաստում",
     id: 1,
   },
   {
     url: clinics,
     title:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    description: "What is Lorem Ipsum?",
+    description: "Ալեքսանդրիտային մազահեռացում",
     id: 2,
   },
   {
     url: clinics,
     title:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    description: "What is Lorem Ipsum?",
+    description: "Կոսմետոլոգիա",
     id: 3,
   },
   {
     url: clinics,
     title:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    description: "What is Lorem Ipsum?",
+    description: "Սոլյարի",
     id: 4,
   },{
     url: clinics,
