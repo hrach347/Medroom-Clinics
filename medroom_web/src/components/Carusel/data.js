5073865213423357307acf319f05a6dc019bd02d
ZAQARYANARMAN <azaqaryan781@gmail.com>
2025-03-13 17:53:59 +0400

    ays branchy footer i hamar e

--- medroom_web/src/components/Carusel/data.js
+++ medroom_web/src/components/Carusel/data.js
@@ -1,33 +1,33 @@
-import styles from "./style.module.css";
+// import styles from "./style.module.css";
 
 
-export const settings = {
-  draggable: false,
-  infinite: true,
-  autoPlay: true,
-  showDots: false,
-  arrows: false,
-  containerClass: styles.slider,
-  itemClass: styles.item,
-  autoPlaySpeed: 20,
-  customTransition: "all 2000ms linear",
-  transitionDuration: 2000,
-  responsive: {
-    superLargeDesktop: {
-      breakpoint: { max: 4000, min: 3000 },
-      items: 4,
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3,
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 650 },
-      items: 2,
-    },
-    mobile: {
-      breakpoint: { max: 600, min: 0 },
-      items: 1,
-    },
-  },
-};
+// export const settings = {
+//   draggable: false,
+//   infinite: true,
+//   autoPlay: true,
+//   showDots: false,
+//   arrows: false,
+//   containerClass: styles.slider,
+//   itemClass: styles.item,
+//   autoPlaySpeed: 20,
+//   customTransition: "all 2000ms linear",
+//   transitionDuration: 2000,
+//   responsive: {
+//     superLargeDesktop: {
+//       breakpoint: { max: 4000, min: 3000 },
+//       items: 4,
+//     },
+//     desktop: {
+//       breakpoint: { max: 3000, min: 1024 },
+//       items: 3,
+//     },
+//     tablet: {
+//       breakpoint: { max: 1024, min: 650 },
+//       items: 2,
+//     },
+//     mobile: {
+//       breakpoint: { max: 600, min: 0 },
+//       items: 1,
+//     },
+//   },
+// };
--- medroom_web/src/ui/Footer/index.jsx
+++ medroom_web/src/ui/Footer/index.jsx
@@ -1,9 +1,19 @@
 import styles from "./style.module.css";
+import FooterPartOne from "../../components/FooterPartOne";
+import FooterPartTwo from "../../components/FooterPartTwo";
+import FooterPartThree from "../../components/FooterPartTree";
 
 const Footer = () => {
   return (
     <div className={styles.container}>
-      INFO
+      <div className={styles.footerTopPartContainer}>
+        <FooterPartOne />
+       <FooterPartTwo />
+       <FooterPartThree />
+      </div>
+      <hr className={styles.bottomLine} />
+      <h3 className={styles.bottomTitle}>lorem lorem lorem lorem</h3>
+      <p className={styles.bottomText}>lorem</p>
     </div>
   );
 };
