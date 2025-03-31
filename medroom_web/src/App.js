f5e2a2f091d7148949954867ab4d77e42167364f
hrach347 <hrachpapyan347@gmail.com>
2025-03-31 20:16:15 +0400

    fixed routes, added Scroll Restoration

--- medroom_web/src/App.js
+++ medroom_web/src/App.js
@@ -1,11 +1,8 @@
 import { Route, Routes } from "react-router";
-import Home from "./pages/Home";
-import Contact from "./pages/Contact";
-import Services from "./pages/Services";
-import About from "./pages/About";
 import Header from "./ui/Header";
-import Staff from "./pages/Staff";
 import Footer from "./ui/Footer";
+import routes from "./Routes.js";
+
 import "./App.css";
 
 function App() {
@@ -13,11 +10,9 @@ function App() {
     <div className="container">
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/Services" element={<Services />} />
-        <Route path="/Staff" element={<Staff />} />
-        <Route path="/Contact" element={<Contact />} />
+        {routes.map((route, index) => (
+          <Route path={route.path} element={route.element} key={index} />
+        ))}
       </Routes>
       <Footer />
     </div>
--- medroom_web/src/index.js
+++ medroom_web/src/index.js
@@ -1,10 +1,12 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import App from "./App";
+import ScrollRestoration from "./helpers/ScrollRestoration";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
+    <ScrollRestoration />
     <App />
   </BrowserRouter>
 );
