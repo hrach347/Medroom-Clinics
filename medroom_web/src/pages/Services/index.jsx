448b9925bcefa6deb40ffde2cc9710046f510fc7
SaqoAxayan <saqoaxayan31@gmail.com>
2025-04-14 23:59:34 +0400

    productionInfo change

--- medroom_web/src/pages/Services/index.jsx
+++ medroom_web/src/pages/Services/index.jsx
@@ -1,8 +1,9 @@
-import { pendingCards } from "./data";
+import { pendingCards, productionData } from "./data";
 import styles from "./style.module.css";
 import Title from "../../components/Title";
 import WelcomeCard from "../../components/WelcomeCard";
 import { useParams } from "react-router";
+import ProductionInfo from "../../ui/ProductionInfo";
 
 function Services() {
 
@@ -13,6 +14,7 @@ function Services() {
       <div className={styles.serviceCards}>
         {pendingCards}
       </div>
+      <ProductionInfo data={productionData}/>
     </div>
   )
 }
