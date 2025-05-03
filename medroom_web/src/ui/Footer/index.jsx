b92eeb7edced77913014d220b7da91391a311351
SaqoAxayan <saqoaxayan31@gmail.com>
2025-05-03 16:43:33 +0400

    services foto update fother icons update

--- medroom_web/src/ui/Footer/index.jsx
+++ medroom_web/src/ui/Footer/index.jsx
@@ -1,22 +1,35 @@
 import styles from "./style.module.css";
 import { Link } from "react-router";
 import { useTranslation } from "react-i18next";
+import { clinicData, linkIcons } from "./data";
 
 const Footer = () => {
+  const { t } = useTranslation("footer");
+  const pages = t("pages", { returnObjects: true });
+  const titles = t("titles", { returnObjects: true });
 
-  const { t } = useTranslation('footer');
+  const pendingDataIcons = clinicData.map((element, index) => {
+    return (
+      <h4 className={styles.clinicDataTitle} key={index + "" + element.text}>
+        <img src={element.icon} alt="" className={styles.logo2} />
+        {element.text}
+      </h4>
+    );
+  });
 
-  const linkIcons = ["", "", "", ""]
+  const pendingDataMore = pages.map((element, index) => {
+    return (
+      <li className={styles.linkList} key={index}>
+        <Link to={element.link} key={index}>
+          {element.name}
+        </Link>
+      </li>
+    );
+  });
 
-
-  const clinicData = [
-    { icon: '/Assets/icons/location.svg', text: "11 Sayat-Nova Ave, Yerevan 0001", },
-    { icon: '/Assets/icons/telephone.svg', text: "telephone number", },
-    { icon: '/Assets/icons/mail.svg', text: "clinic mail", },
-  ]
-
-  const pages = t('pages', {returnObjects: true});
-  const titles = t('titles', {returnObjects: true});
+  const pendingIcons = linkIcons.map((el) => <a key={el.id} href={el.path} target="_blank">
+     <img src={el.icon} alt=""/>
+  </a>);
 
   return (
     <div className={styles.container}>
@@ -24,34 +37,19 @@ const Footer = () => {
         <div className={styles.partContainer}>
           <h3>{titles.contacts}</h3>
           <hr />
-          <img src='/Assets/Images/logo.png' alt="" width="120px" height="62px" className={styles.logo} />
-          {
-            clinicData.map((element, index) => {
-              return (
-                <h4 className={styles.clinicDataTitle} key={index + "" + element.text}>
-                  <img src={element.icon} alt="" width="24px" height="24px" />
-                  {element.text}
-                </h4>
-              )
-            })
-          }
+          <img src="/Assets/Images/logo.png" alt="" className={styles.logo} />
+          {pendingDataIcons}
         </div>
         <div className={styles.partContainer}>
           <h3 className={styles.footerPartOneTitle}>{titles.map}</h3>
           <hr className={styles.line} />
-          <ul className={styles.list}>
-            {
-              pages.map((element, index) => {
-                return <li className={styles.linkList} key={index}><Link to={element.link} key={index}>{element.name}</Link></li>
-              })
-            }
-          </ul>
+          <ul className={styles.list}>{pendingDataMore}</ul>
         </div>
         <div className={styles.partContainer}>
           <h3>{titles.social}</h3>
           <hr />
           <div className={styles.iconsContainer}>
-           
+            {pendingIcons}
           </div>
         </div>
       </div>
